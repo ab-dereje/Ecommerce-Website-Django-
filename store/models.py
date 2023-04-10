d566ad592af4ebb35f963f038fb9301e6f8c4e84
Abdi Dereje <abudereje09@gmail.com>
2023-04-10 17:47:00 -0400

    5th commit

--- store/models.py
+++ store/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+class ProductManager(models.Manager):
+    def get_queryset(self):
+        return super(ProductManager, self).get_queryset().filter(is_active=True)
+
 class Category(models.Model):
     name = models.CharField(max_length=255,db_index=True)
     slug = models.SlugField(max_length=255, unique=True)
@@ -27,6 +31,8 @@ class Product(models.Model):
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
+    products = ProductManager()
 
     class Meta:
         verbose_name_plural = 'products'
